4018e38b776fe655091719a514f10b0bc0e68586
Ayomipe <oloniluaayomipe@gmail.com>
2023-09-19 15:06:31 +0100

    add section tag of bootstrap container

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
-$(document).ready(function() {
+// Business Logic
 
+      function reverse(word) {
+        const split = word.split('');
+        const reversedArray = [];
+      
+        split.forEach(function(element) {
+          reversedArray.unshift(element); 
+        });
+      
+        const reversedWord = reversedArray.join('');
+        return reversedWord;
+      }
+      function wordPlay(word) {
+        let originalWordPlay = word;
+        if (word.length >= 3) {
+            let reversedWordPlay  = reverse(word) + ' ' + word;
+            return reversedWordPlay;
+        } else {
+            return originalWordPlay;
+        }
+      }
+      
+    
+// UI Logic
+
+$(document).ready(function() {
+    $("#word").submit(function(e) {
+        e.preventDefault();
+        
+        let word = $("#wordPlay").val();
+        let result = wordPlay(word);
+    
+        $("#result").html(result);
+
+    });
 });
+
